f202e3cb6c9b620713f4aa3b1e86cbc034ffd3cd
Claire Miller <clairemiller@claires-mbp.lan>
2017-08-22 19:13:56 -0500

    basic styling done, prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/views'));
 
@@ -7,6 +8,6 @@ app.get('/', function(req, res) {
 	res.sendFile('index.html');
 });
 
-app.listen(5000, function() {
+app.listen(port, function() {
 	console.log('Server listening on port 5000');
 });
