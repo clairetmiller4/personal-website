621bfacac29da05b1778b459d691834c14f59770
Claire Miller <clairemiller@Claires-MacBook-Pro.local>
2019-01-20 15:23:13 -0600

    fixing errors from history mode

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const history = require('connect-history-api-fallback');
 
 const app = express();
-
 const port = process.env.PORT || 8080;
+const staticAssets = express.static(path.join(__dirname, 'dist'));
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(staticAssets);
+
+// Fixing navigation errors associated with HTML5 history mode
+app.use(history());
+
+// Must include static assets again as described in documentation
+// https://github.com/bripkens/connect-history-api-fallback/tree/master/examples/static-files-and-index-rewrite#configuring-the-middleware
+app.use(staticAssets);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
